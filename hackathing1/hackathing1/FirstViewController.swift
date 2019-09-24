db2fe7a6246a931f56315a3713b939e2d75bf890
Shoshana <Shoshana.M.Geller.20@dartmouth.edu>
2019-09-24 00:03:38 -0400

    beginning of swift project

---
+++ hackathing1/hackathing1/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  hackathing1
+//
+//  Created by Shoshana Geller on 9/23/19.
+//  Copyright © 2019 Shoshana Geller. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello, world!")
+    }
+
+
+}
+
---
+++ hackathing1/hackathing1/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  hackathing1
+//
+//  Created by Shoshana Geller on 9/23/19.
+//  Copyright © 2019 Shoshana Geller. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
